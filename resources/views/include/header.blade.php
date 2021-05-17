95ff52871b310a8e6e74b26e6cec48a7cdd277f9
soomahouraiby <asoomahuraiby@gmail.com>
2021-05-18 00:22:55 +0300

    =kkkkkkkkkk

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -51,40 +51,40 @@
                 @auth
                 @if(auth()->user()->hasRole('مدير الصيدلة'))
                     <li class="dropdown dropdown-notification nav-item  dropdown-notifications" >
-                        <a class="nav-link nav-link-label" href="#" data-toggle="dropdown" >
+                        <a class="nav-link nav-link-label" href="{{route('PM_newReports')}}" data-toggle="dropdown" >
                             <i class="fa fa-bell" style="color: white"> </i>
                             <span
                                 class="badge badge-pill badge-default badge-danger badge-default badge-up badge-glow
                                  notif-count" data-count="0" style="background-color: white ; color: #0F122D">0</span>
                         </a>
-                               <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right" style="background-color: white ; color: #0F122D">
-                                                    <li class="dropdown-menu-header">
-                                                        <h6 class="dropdown-header m-0 text-center" style="background-color: white ; color: #0F122D">
-                                                            <span class="grey darken-2 text-center" style="background-color: white ; color: #0F122D"> الرسائل</span>
-                                                        </h6>
-                                                    </li>
-                                                    <li class="scrollable-container ps-container ps-active-y media-list w-100" style="background-color: white ; color: #0F122D">
-                                                        <a href="">
-                                                            <div class="media" style="background-color: white ; color: #0F122D">
-                                                                <div class="media-body" style="background-color: white ; color: #0F122D">
-                                                                    <h6 class="media-heading text-right " style="background-color: white ; color: #0F122D">عنوان الاشعار </h6>
-                                                                    <p class="notification-text font-small-3 text-muted text-right" style="background-color: white ; color: #0F122D"> نص الاشعار</p>
-                                                                    <small style="direction: ltr;background-color: white ; color: #0F122D">
-                                                                        <p class=" text-muted text-right"
-                                                                           style="direction: ltr; background-color: white ; color: #0F122D"> 20-05-2020 - 06:00 pm
-                                                                        </p>
-                                                                        <br>
+{{--                               <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right" style="background-color: white ; color: #0F122D">--}}
+{{--                                                    <li class="dropdown-menu-header">--}}
+{{--                                                        <h6 class="dropdown-header m-0 text-center" style="background-color: white ; color: #0F122D">--}}
+{{--                                                            <span class="grey darken-2 text-center" style="background-color: white ; color: #0F122D"> الرسائل</span>--}}
+{{--                                                        </h6>--}}
+{{--                                                    </li>--}}
+{{--                                                    <li class="scrollable-container ps-container ps-active-y media-list w-100" style="background-color: white ; color: #0F122D">--}}
+{{--                                                        <a href="">--}}
+{{--                                                            <div class="media" style="background-color: white ; color: #0F122D">--}}
+{{--                                                                <div class="media-body" style="background-color: white ; color: #0F122D">--}}
+{{--                                                                    <h6 class="media-heading text-right " style="background-color: white ; color: #0F122D">عنوان الاشعار </h6>--}}
+{{--                                                                    <p class="notification-text font-small-3 text-muted text-right" style="background-color: white ; color: #0F122D"> نص الاشعار</p>--}}
+{{--                                                                    <small style="direction: ltr;background-color: white ; color: #0F122D">--}}
+{{--                                                                        <p class=" text-muted text-right"--}}
+{{--                                                                           style="direction: ltr; background-color: white ; color: #0F122D"> 20-05-2020 - 06:00 pm--}}
+{{--                                                                        </p>--}}
+{{--                                                                        <br>--}}
 
-                                                                    </small>
-                                                                </div>
-                                                            </div>
-                                                        </a>
+{{--                                                                    </small>--}}
+{{--                                                                </div>--}}
+{{--                                                            </div>--}}
+{{--                                                        </a>--}}
 
-                                                    </li>
-                                                    <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"style="background-color: white ; color: #0F122D"
-                                                                                        href=""> جميع الاشعارات </a>
-                                                    </li>
-                                                </ul>
+{{--                                                    </li>--}}
+{{--                                                    <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"style="background-color: white ; color: #0F122D"--}}
+{{--                                                                                        href=""> جميع الاشعارات </a>--}}
+{{--                                                    </li>--}}
+{{--                                                </ul>--}}
                     </li>
 
                     @endif
